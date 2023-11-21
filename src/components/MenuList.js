d7e0f795bc6c9507edb293c356c2cd3f20a5a961
Jafari Momtaz, Farshad <jafari_f@484938T1085598>
2023-11-21 11:59:05 +0330

    menu clicking corrected

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -2,17 +2,21 @@ import React from 'react';
 import {BiMenu} from "react-icons/bi";
 import MenuListItems from "./MenuListItems";
 import menuListData from "../menuListData";
-function MenuList(props) {
+function MenuList() {
     const [menuContent,setMenuContent] = React.useState(false)
     console.log(menuContent)
+    function closeModal() {
+        setMenuContent(prevState => !prevState)
+    }
     return (
-        <div className='main-container'>
-            <div className='menu-container' onClick={() => setMenuContent(!menuContent)}>
-            <BiMenu className={menuContent?'menu-container-show':'menu-icon'}/>
-        </div>
-            <div className={menuContent?'menu-items-container':'menu-container-hide'}>
+        <div className='main-container' >
+            <div className='menu-container' onClick={closeModal}>
+                  <BiMenu className={menuContent?'menu-container-show':'menu-icon'}/>
+            </div>
+            <div className={menuContent?'menu-items-container':'menu-container-hide'} onClick={closeModal}>
                 {menuListData.map((menuItem) => {
                         return <MenuListItems
+                            onClick={e => e.stopPropagation()}
                             title={menuItem.title}
                             key={menuItem.id}
                         />
--- src/components/MenuListItems.js
+++ src/components/MenuListItems.js
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function MenuListItems(props) {
     return (
-        <div>
             <div className='menu-list-items' id={props.id}>{props.title}</div>
-        </div>
     )
 }
